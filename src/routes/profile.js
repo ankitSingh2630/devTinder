95025161ac62e335af4c004f88d18b5447b310c0
ankitsinghtechnians <ankit.singh@technians.com>
2026-01-28 12:46:59 +0530

    reset-password successfully

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const userAuth=require('../middlewares/auth')
-const {validateEditProfile}= require('../utils/signupValidation')
+const {validateEditProfile}= require('../utils/signupValidation');
+const User = require('../models/user');
+const bcrypt=require('bcrypt');
 
 const profileRouter = express.Router();
 
@@ -14,6 +16,7 @@ profileRouter.get("/profile/view",userAuth,async(req,res)=>{
     }
 })
 
+// profile edit
 profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
     try{
 
@@ -34,5 +37,30 @@ profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
         res.status(400).send("ERROR: "+ err.message); 
     }
 })
+
+// forgot password
+profileRouter.patch("/profile/password",userAuth,async(req,res)=>{
+    const {previousPassword,password}=req.body;
+
+    const loggedInUser=req.user;
+
+    const checkPassCorrect = await loggedInUser.validatePassword(previousPassword);  
+     if(!checkPassCorrect){
+        throw new Error("Please enter correct email and password");
+    }     
+   
+    const hashedPassword = await bcrypt.hash(password,10);
+
+     loggedInUser.password=hashedPassword;
+     await loggedInUser.save();
+
+     res.send("password has been changed successfully");
+
+})
+
+
+// previos password - req.body
+
+
 
 module.exports= profileRouter;
